be584092f4c3ac30314a8735439e94e26c7b5da1
Delphi FPC Lazarus cross compiling Toolbox <support@peter-ebe.de>
2023-06-27 22:02:20 +0200

    Update collectdata.sh
    enhanced collectdatascript

--- measurementscripts/collectdata.sh
+++ measurementscripts/collectdata.sh
@@ -3,14 +3,31 @@
 # Vorbereiten
 rm /home/pi/data/*
 
-# Resol Messdaten abfragen
-cd /home/pi/resol
-python /home/pi/resol/resol.py > /home/pi/data/resol.json
+# vzlogger abfragen
+cd /home/pi/data/
+wget http://192.168.1.252:8080/
+mv index.html vzlogger.json
 
 # Stecagrid Messdaten abfragen
 cd /home/pi/data/
 wget http://192.168.1.251/measurements.xml
 mv measurements.xml stecagrid.xml
+
+# ESP Powermeter Messdaten abfragen
+cd /home/pi/data/
+wget http://192.168.1.240/avgDo
+mv avgDo esppowermeter.result 
+sleep 10
+wget http://192.168.1.240/avgFetchPower
+mv avgFetchPower esppowermeter.data
+
+# Resol Messdaten abfragen
+cd /home/pi/resol
+python /home/pi/resol/resol.py > /home/pi/data/resol.json
+
+# -------------------------------------------------
+
+# ggf. daten anderweitig verarbeiten
 
 # -------------------------------------------------
 
