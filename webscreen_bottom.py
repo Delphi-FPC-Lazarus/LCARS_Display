a6b40dd31dc93d8b3e64d2e887c5e7575192989f
Delphi FPC Lazarus cross compiling Toolbox <support@peter-ebe.de>
2023-09-09 01:21:28 +0200

    Update webscreen_bottom.py
    python 3.9.*

--- webscreen_bottom.py
+++ webscreen_bottom.py
@@ -73,29 +73,25 @@ with open("/home/pi/data/resol.json") as json_format_file:
 resoljsonroot = resoljson["DeltaSol BS 2009"]
 
 # Fuehler oben (entities 14 0d1da960-a014-11ea-985e-e9d8eac2fe2e)
-value = resoljsonroot["Temperatur Sensor 3"]
-value = value.encode('utf8').replace('\xc2', '').replace('\xb0','&deg;');
+value = resoljsonroot["Temperatur Sensor 3"].replace('\xc2', '').replace('\xb0','&deg;');
 print("<tr><td width=250>Wasser:</td><td>");
 print(value);
 print("</td/tr>");
 
 # Fuehler unten (entities 13 1c26b290-97c9-11ea-9c01-11ed7286b899)
-value = resoljsonroot["Temperatur Sensor 2"]
-value = value.encode('utf8').replace('\xc2', '').replace('\xb0','&deg;');
+value = resoljsonroot["Temperatur Sensor 2"].replace('\xc2', '').replace('\xb0','&deg;');
 print("<tr><td></td><td>");
 print(value);
 print("</td/tr>");
 
 # Kesseltemperatur (entities 15 74e335c0-c94e-11eb-a752-512e21a0807b)
-value = resoljsonroot["Temperatur Sensor 4"]
-value = value.encode('utf8').replace('\xc2', '').replace('\xb0','&deg;');
+value = resoljsonroot["Temperatur Sensor 4"].replace('\xb0','&deg;');
 print("<tr><td>Kessel:</td><td>");
 print(value);
 print("</td/tr>");
 
 # Kollektortemperatur (entities 16 2ed03d20-c950-11eb-a986-fd2195d4a96e)
-value = resoljsonroot["Temperatur Sensor 1"]
-value = value.encode('utf8').replace('\xc2', '').replace('\xb0','&deg;');
+value = resoljsonroot["Temperatur Sensor 1"].replace('\xb0','&deg;');
 print("<tr><td>Kollektor:</td><td>");
 print(value);
 print("</td/tr>");
